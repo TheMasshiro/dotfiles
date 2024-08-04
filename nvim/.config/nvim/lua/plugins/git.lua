26835f0ef100e59cb528c5316850ce335502756f
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-04 17:28:41 +0800

    add(gitgraph): added gitgraph

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -51,4 +51,24 @@ return {
 		"tpope/vim-fugitive",
 		event = { "BufReadPre", "BufNewFile" },
 	},
+	{
+		"isakbm/gitgraph.nvim",
+		dependencies = { "sindrets/diffview.nvim" },
+		---@type I.GGConfig
+		opts = {
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+		},
+		init = function()
+			vim.keymap.set("n", "<leader>gg", function()
+				require("gitgraph").draw({}, { all = true, max_count = 5000 })
+			end, { desc = "Git Graph" })
+		end,
+	},
 }
