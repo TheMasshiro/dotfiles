99725cef53aa5fbd4efe8b991ff365f6c82e0f9a
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-09 12:27:29 +0800

    add: based on checkhealth

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -1,10 +1,13 @@
 return {
 	"rmagatti/auto-session",
+	lazy = false,
 	config = function()
 		require("auto-session").setup({
 			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "~/Desktop", "/" },
 		})
+
+		vim.o.sessionoptions = "localoptions"
 
 		vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session (Session)" })
 		vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session (Session)" })
