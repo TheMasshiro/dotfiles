d59c480ef26d507ff2b5ad73b5d4396092900fc9
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-09 12:27:58 +0800

    add: cmd

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -6,6 +6,7 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
+	cmd = "Neotree",
 	keys = {
 		{ "<leader>fne", "<cmd>Neotree toggle<CR>", desc = "Toggle file tree (Neotree)" },
 		{
