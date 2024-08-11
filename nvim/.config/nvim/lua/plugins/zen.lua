c0aa1ce1f28b7648a484cc70037334e1d8b5d46b
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-11 14:38:51 +0800

    add zen

---
+++ nvim/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/zen-mode.nvim",
+	keys = {
+		{
+			"<leader>zz",
+			"<cmd>ZenMode<CR>",
+			desc = "Toggle Zen Mode",
+		},
+	},
+	opts = {
+		window = {
+			width = 0.9,
+		},
+	},
+}
