42a5c1b1e3a34c0ccae248cb2c5886e739043176
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-18 18:51:36 +0800

    Lazy load
    lazy load neotest with filetype

--- nvim/.config/nvim/lua/plugins/testing.lua
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -1,93 +1,108 @@
 return {
-	"nvim-neotest/neotest",
-	ft = { "go", "python" },
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		-- Adapters
-		{
-			"fredrikaverpil/neotest-golang",
-			ft = "go",
+	-- Lazy load adapters depending on filetype
+	-- Adapters
+	{
+		"fredrikaverpil/neotest-golang",
+		ft = "go",
+		dependencies = {
+			"nvim-neotest/neotest",
 		},
-		{
-			"nvim-neotest/neotest-python",
-			ft = "python",
-		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang"),
+				},
+			})
+		end,
 	},
-	keys = {
-		{ "<leader>t", "", desc = "+test" },
-		{
-			"<leader>tt",
-			function()
-				require("neotest").run.run(vim.fn.expand("%"))
-			end,
-			desc = "Run File",
+	{
+		"nvim-neotest/neotest-python",
+		ft = "python",
+		dependencies = {
+			"nvim-neotest/neotest",
 		},
-		{
-			"<leader>tT",
-			function()
-				require("neotest").run.run(vim.uv.cwd())
-			end,
-			desc = "Run All Test Files",
-		},
-		{
-			"<leader>tr",
-			function()
-				require("neotest").run.run()
-			end,
-			desc = "Run Nearest",
-		},
-		{
-			"<leader>tl",
-			function()
-				require("neotest").run.run_last()
-			end,
-			desc = "Run Last",
-		},
-		{
-			"<leader>ts",
-			function()
-				require("neotest").summary.toggle()
-			end,
-			desc = "Toggle Summary",
-		},
-		{
-			"<leader>to",
-			function()
-				require("neotest").output.open({ enter = true, auto_close = true })
-			end,
-			desc = "Show Output",
-		},
-		{
-			"<leader>tO",
-			function()
-				require("neotest").output_panel.toggle()
-			end,
-			desc = "Toggle Output Panel",
-		},
-		{
-			"<leader>tS",
-			function()
-				require("neotest").run.stop()
-			end,
-			desc = "Stop",
-		},
-		{
-			"<leader>tw",
-			function()
-				require("neotest").watch.toggle(vim.fn.expand("%"))
-			end,
-			desc = "Toggle Watch",
-		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-python"),
+				},
+			})
+		end,
 	},
-	config = function()
-		require("neotest").setup({
-			adapters = {
-				require("neotest-golang"),
-				require("neotest-python"),
+	{
+		"nvim-neotest/neotest",
+		ft = { "go", "python" },
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{ "<leader>t", "", desc = "+test" },
+			{
+				"<leader>tt",
+				function()
+					require("neotest").run.run(vim.fn.expand("%"))
+				end,
+				desc = "Run File",
 			},
-		})
-	end,
+			{
+				"<leader>tT",
+				function()
+					require("neotest").run.run(vim.uv.cwd())
+				end,
+				desc = "Run All Test Files",
+			},
+			{
+				"<leader>tr",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Run Nearest",
+			},
+			{
+				"<leader>tl",
+				function()
+					require("neotest").run.run_last()
+				end,
+				desc = "Run Last",
+			},
+			{
+				"<leader>ts",
+				function()
+					require("neotest").summary.toggle()
+				end,
+				desc = "Toggle Summary",
+			},
+			{
+				"<leader>to",
+				function()
+					require("neotest").output.open({ enter = true, auto_close = true })
+				end,
+				desc = "Show Output",
+			},
+			{
+				"<leader>tO",
+				function()
+					require("neotest").output_panel.toggle()
+				end,
+				desc = "Toggle Output Panel",
+			},
+			{
+				"<leader>tS",
+				function()
+					require("neotest").run.stop()
+				end,
+				desc = "Stop",
+			},
+			{
+				"<leader>tw",
+				function()
+					require("neotest").watch.toggle(vim.fn.expand("%"))
+				end,
+				desc = "Toggle Watch",
+			},
+		},
+	},
 }
