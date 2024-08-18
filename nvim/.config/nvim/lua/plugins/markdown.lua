c88944e92558bd190460ec3cc0466f4ebf2c3a1f
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-18 18:51:16 +0800

    Add  markdown preview
    lazy load to markdown and added markdown preview for easier
    visualization

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,13 +1,24 @@
 return {
-	"MeanderingProgrammer/markdown.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	config = function()
-		require("render-markdown").setup({
-			latex = {
-				enabled = false,
-			},
-		})
-	end,
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		ft = "markdown",
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		config = function()
+			require("render-markdown").setup({
+				latex = {
+					enabled = false,
+				},
+			})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = "markdown",
+	},
 }
