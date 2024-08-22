02501a9beead5dfd9737977018fe0d958634b893
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-22 15:51:30 +0800

    refactor: improve keymap for clearing search highlight

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,8 +1,16 @@
 local keymap = vim.keymap
 
-keymap.set({ "i", "n" }, "<esc>", "<esc>:nohlsearch<CR>", { silent = true, desc = "clear search highlight" })
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selected lines down" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move selected lines up" })
 
-keymap.set("n", "<leader>rr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Rename all variables" })
+keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
