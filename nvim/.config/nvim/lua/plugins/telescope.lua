1aa0a1d6313b8414791ed6122eee2b4368f5de53
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-22 16:00:16 +0800

    feat: add telescope keybindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,20 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"folke/todo-comments.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -16,24 +25,49 @@ return {
 				path_display = { "smart" },
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
 					},
 				},
 			},
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
 		})
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
 
-		telescope.load_extension("fzf")
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
+		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+		vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "[G]it [F]iles" })
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+		vim.keymap.set("n", "<leader>f/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "/ [F]uzzily search in current buffer" })
 
-		keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files (Telescope)" })
-		keymap.set("n", "<leader>fg", ":Telescope git_files<CR>", { desc = "Git Files (Telescope)" })
-		keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Recent Files (Telescope)" })
-		keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Grep Search (Telescope)" })
-		keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Grep search under cursor (Telescope)" })
-		keymap.set("n", "<leader>ft", "<:>TodoTelescope<CR>", { desc = "Find Todos (Todo)" })
+		vim.keymap.set("n", "<leader>fo", function()
+			builtin.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live Grep in Open Files",
+			})
+		end, { desc = "[F]ind / in [O]pen Files" })
+
+		vim.keymap.set("n", "<leader>fn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[F]ind [N]eovim files" })
 	end,
 }
