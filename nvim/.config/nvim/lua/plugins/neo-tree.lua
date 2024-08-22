3aa0c95390f8c2c4cd4c7e55c7f5dd0ee8e0f5e9
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-22 15:58:39 +0800

    feat: add neo-tree plugin

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,45 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{
+			"\\f",
+			":Neotree reveal<CR>",
+			desc = "Neotree: [F]ile tree",
+		},
+		{ "\\g", ":Neotree toggle git_status<CR>", desc = "Neotree: [G]it tree" },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+			},
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+		close_if_last_window = true,
+		default_component_configs = {
+			indent = {
+				with_expanders = true,
+				expander_collapsed = "",
+				expander_expanded = "",
+				expander_highlight = "NeoTreeExpander",
+			},
+		},
+		git_status = {
+			symbols = {
+				unstaged = "󰄱",
+				staged = "󰱒",
+			},
+		},
+	},
+}
