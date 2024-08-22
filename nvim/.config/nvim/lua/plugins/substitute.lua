9b157949cd258b82e058786750bff74ac3a87c79
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-22 16:00:06 +0800

    refactor: remove unnecessary  function

--- nvim/.config/nvim/lua/plugins/substitute.lua
+++ nvim/.config/nvim/lua/plugins/substitute.lua
@@ -1,17 +1,39 @@
 return {
 	"gbprod/substitute.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local substitute = require("substitute")
-
-		substitute.setup()
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>s", substitute.operator, { desc = "Substitute with motion" })
-		keymap.set("n", "<leader>ss", substitute.line, { desc = "Substitute line" })
-		keymap.set("n", "<leader>sS", substitute.eol, { desc = "Substitute to end of line" })
-		keymap.set("x", "<leader>s", substitute.visual, { desc = "Substitute in visual mode" })
-	end,
+	keys = {
+		{
+			"<leader>s",
+			function()
+				require("substitute").operator()
+			end,
+			mode = { "n" },
+			desc = "[S]ubstitute with motion",
+		},
+		{
+			"<leader>ss",
+			function()
+				require("substitute").line()
+			end,
+			mode = { "n" },
+			desc = "[S]ub[S]titute line",
+		},
+		{
+			"<leader>se",
+			function()
+				require("substitute").eol()
+			end,
+			mode = { "n" },
+			desc = "[S]ubstitut[E] to end of line",
+		},
+		{
+			"<leader>s",
+			function()
+				require("substitute").visual()
+			end,
+			mode = { "x" },
+			desc = "[S]ubstitute in visual mode",
+		},
+	},
+	opts = {},
 }
