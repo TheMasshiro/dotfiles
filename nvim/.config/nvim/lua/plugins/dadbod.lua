b6b0a3b3671be15225c2f08f7cd1fb4ce5053b50
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-30 10:06:05 +0800

    refactor: update keymap descriptions to use Dadbod

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -14,7 +14,7 @@ return {
 		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.g.db_ui_show_help = 0
-		vim.keymap.set("n", "<leader>no", ":DBUI<CR>", { desc = "[O]pen Database" })
-		vim.keymap.set("n", "<leader>nt", ":DBUIToggle<CR>", { desc = "[T]oggle Database" })
+		vim.keymap.set("n", "<leader>no", ":DBUI<CR>", { desc = "[O]pen Dadbod" })
+		vim.keymap.set("n", "<leader>nt", ":DBUIToggle<CR>", { desc = "[T]oggle Dadbod" })
 	end,
 }
