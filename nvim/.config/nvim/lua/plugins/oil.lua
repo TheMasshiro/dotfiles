46c37682a35c3e6f416b64be8de6ef80c543fd85
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-30 10:08:38 +0800

    refactor: configure oil explorer with hidden files

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,15 @@
 return {
 	"stevearc/oil.nvim",
-	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local oil = require("oil")
 		oil.setup({
 			keymaps = {
 				["\\"] = { callback = "actions.close", mode = "n" },
+			},
+
+			view_options = {
+				show_hidden = true,
 			},
 		})
 		vim.keymap.set("n", "\\e", oil.toggle_float, { desc = "Oil: Open [E]xplorer" })
