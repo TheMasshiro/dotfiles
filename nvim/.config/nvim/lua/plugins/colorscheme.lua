41703d171b6b83e19b61e167b6894123aebb7b26
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-08-30 10:05:16 +0800

    refactor: move catppuccin config to opts

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,27 +2,29 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			no_italic = true,
-			term_colors = true,
-			transparent_background = false,
-			styles = {
-				comments = {},
-				conditionals = {},
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-			},
-		})
-		vim.cmd.colorscheme("catppuccin")
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-	end,
+	opts = {
+		flavour = "mocha", -- latte, frappe, macchiato, mocha, or auto
+		no_italic = true,
+		term_colors = true,
+		transparent_background = false,
+		styles = {
+			comments = {},
+			conditionals = {},
+			loops = {},
+			functions = {},
+			keywords = {},
+			strings = {},
+			variables = {},
+			numbers = {},
+			booleans = {},
+			properties = {},
+			types = {},
+		},
+
+		function()
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
