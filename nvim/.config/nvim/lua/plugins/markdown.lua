42cbdcd502a7fe74cd399bd56e5410ca3d6aefcb
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-03 08:46:44 +0800

    refactor: remove unused markdown plugin configurations

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,24 +1,14 @@
 return {
-	{
-		"MeanderingProgrammer/markdown.nvim",
-		ft = "markdown",
-		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-		config = function()
-			require("render-markdown").setup({
-				latex = {
-					enabled = false,
-				},
-			})
-		end,
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = "markdown",
+	"OXY2DEV/markview.nvim",
+	lazy = false, -- Recommended
+	-- ft = "markdown" -- If you decide to lazy-load anyway
+
+	dependencies = {
+		-- You will not need this if you installed the
+		-- parsers manually
+		-- Or if the parsers are in your $RUNTIMEPATH
+		"nvim-treesitter/nvim-treesitter",
+
+		"nvim-tree/nvim-web-devicons",
 	},
 }
