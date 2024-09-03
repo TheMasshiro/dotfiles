3c3b7c298db817856a219175fa8899a9cf8f0288
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-03 18:39:09 +0800

    refactor: changed keybindings

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -83,14 +83,14 @@ return {
 				dapui.eval(nil, { enter = true })
 			end, { desc = "Debug Evaluate (DAP)" })
 
-			vim.keymap.set("n", "<F9>", dap.continue, { desc = "Debug: Start/Continue" })
 			vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
 			vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
 			vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
 			vim.keymap.set("n", "<F4>", dap.step_back, { desc = "Debug: Step Back" })
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+			vim.keymap.set("n", "<F6>", dap.restart, { desc = "Debug: Restart" })
 			vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result" })
-			vim.keymap.set("n", "<F8>", dap.restart, { desc = "Debug: Restart" })
-			vim.keymap.set("n", "<F10>", dapui.close, { desc = "Debug: Close" })
+			vim.keymap.set("n", "q", dapui.close, { desc = "Debug: Close" })
 
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
