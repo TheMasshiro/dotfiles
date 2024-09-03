0cf2265ccf105b97b23b62cb270d14c0bc2ef56e
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-03 18:38:24 +0800

    refactor: rename lsp document symbol keybinding

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -31,7 +31,7 @@ return {
 					map("<leader>lr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 					map("<leader>lI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ls", require("telescope.builtin").lsp_document_symbols, "Document [S]ymbols")
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
