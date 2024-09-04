f64e317d5f74da6df36884237e3361c0bf50fec8
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-04 10:40:09 +0800

    refactor: move plugins to separate file

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,20 +1,11 @@
 return {
-	"nvim-lua/plenary.nvim",
-
-	"christoomey/vim-tmux-navigator",
-
-	"tpope/vim-sleuth",
-
 	{
 		"stevearc/dressing.nvim",
 		event = "VeryLazy",
 		opts = {},
 	},
-	{
-		"kawre/neotab.nvim",
-		event = "InsertEnter",
-		opts = {
-			-- configuration goes here
-		},
-	},
+
+	"nvim-lua/plenary.nvim",
+	"christoomey/vim-tmux-navigator",
+	"tpope/vim-sleuth",
 }
