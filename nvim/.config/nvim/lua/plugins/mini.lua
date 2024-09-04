828a21707a870bc87b1f9200a22488f8968a5cc5
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-04 10:40:01 +0800

    refactor: simplify plugin setup

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -32,7 +32,6 @@ return {
 
 		-- QOL:
 		require("mini.cursorword").setup()
-		require("mini.pairs").setup()
 
 		-- :Git command for executing any git call inside file's repository root with deeper current instance integration (show output as notification/buffer, use to edit commit messages, etc.).
 		--
