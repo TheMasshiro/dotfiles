dce8e0f04519a447c1b627798986d09ae60e6b48
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-05 09:25:38 +0800

    dunno man

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -2,6 +2,16 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
-		disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil", "neo-tree", "dbui", "dashboard" },
+		disabled_filetypes = {
+			"qf",
+			"netrw",
+			"NvimTree",
+			"lazy",
+			"mason",
+			"oil",
+			"neo-tree",
+			"dbui",
+			"dashboard",
+		},
 	},
 }
