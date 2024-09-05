5e4f24e9259ce5b78e8d7520dedc87404d2745f6
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-09-05 09:25:23 +0800

    refactor: use opts for Harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,25 +2,39 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup()
-
-		vim.keymap.set("n", "<leader>aa", function()
-			harpoon:list():add()
-		end, { desc = "Harpoon: [A]dd" })
-		vim.keymap.set("n", "<leader>ad", function()
-			harpoon:list():remove()
-		end, { desc = "Harpoon: [D]elete" })
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-
-		vim.keymap.set("n", "<C-A-k>", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<C-A-j>", function()
-			harpoon:list():next()
-		end)
-	end,
+	keys = {
+		{
+			"<leader>aa",
+			function()
+				require("harpoon"):list():add()
+			end,
+			desc = "Harpoon: [A]dd",
+		},
+		{
+			"<leader>ad",
+			function()
+				require("harpoon"):list():remove()
+			end,
+			desc = "Harpoon: [A]dd",
+		},
+		{
+			"<C-e>",
+			function()
+				require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+			end,
+		},
+		{
+			"<C-A-p>",
+			function()
+				require("harpoon"):list():prev()
+			end,
+		},
+		{
+			"<C-A-n>",
+			function()
+				require("harpoon"):list():next()
+			end,
+		},
+	},
+	opts = {},
 }
