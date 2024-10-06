83c243d384f642152059e7efb16d13aa2de2417e
John Christian Vicente <johnc.vicente1@gmail.com>
2024-10-06 19:15:54 +0800

    refactor: use recommended installation

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,18 +1,25 @@
 return {
 	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local oil = require("oil")
-		oil.setup({
-			keymaps = {
-				["\\"] = { callback = "actions.close", mode = "n" },
-				["q"] = { callback = "actions.close", mode = "n" },
-			},
-
-			view_options = {
-				show_hidden = true,
-			},
-		})
-		vim.keymap.set("n", "\\e", oil.toggle_float, { desc = "Oil: Open [E]xplorer" })
-	end,
+	---@module 'oil'
+	---@type oil.SetupOpts
+	keys = {
+		{
+			"\\e",
+			function()
+				require("oil").toggle_float()
+			end,
+			desc = "Oil: Open [E]xplorer",
+		},
+	},
+	opts = {
+		keymaps = {
+			["\\"] = { callback = "actions.close", mode = "n" },
+			["q"] = { callback = "actions.close", mode = "n" },
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 }
