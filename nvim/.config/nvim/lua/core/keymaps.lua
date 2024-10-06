bb9c70ec9a77ed8915bdec0660da416dc584fab8
John Christian Vicente <johnc.vicente1@gmail.com>
2024-10-06 19:12:43 +0800

    chore: use vim.keymap for consistent keymap definitions

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,12 +1,15 @@
-local keymap = vim.keymap
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Move highlighted lines up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selected lines down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move selected lines up" })
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selected lines down" })
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move selected lines up" })
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
-keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+-- Terminal mode
+vim.keymap.set("n", "<leader>vv", [[:vsplit | terminal<CR>A]], { desc = "Open terminal in [V]ertical split" })
+vim.keymap.set("n", "<leader>vh", [[:split | terminal<CR>A]], { desc = "Open terminal in [H]orizontal split" })
 
-keymap.set("n", "<leader>vv", [[:vsplit | terminal<CR>A]], { desc = "Open terminal in [V]ertical split" })
-keymap.set("n", "<leader>vh", [[:split | terminal<CR>A]], { desc = "Open terminal in [H]orizontal split" })
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal insert mode" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
