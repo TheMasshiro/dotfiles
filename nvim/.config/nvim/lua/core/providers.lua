e8813e8311ee0f00cdc0450f3124e8e3acf18896
John Christian Vicente <johnc.vicente1@gmail.com>
2024-10-06 19:13:33 +0800

    chore: use vim.g

--- nvim/.config/nvim/lua/core/providers.lua
+++ nvim/.config/nvim/lua/core/providers.lua
@@ -1,5 +1,3 @@
-local provider = vim.g
-
 -- Python Provider
 local python_provider = ".local/share/mise/installs/python/latest/bin/python"
-provider.python3_host_prog = vim.fn.expand("$HOME/") .. python_provider
+vim.g.python3_host_prog = vim.fn.expand("$HOME/") .. python_provider
