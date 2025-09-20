dff6816f72391c49e9278e62eebb21c6c08c3c2d
John Christian Vicente <johnc.vicente1@gmail.com>
2025-09-20 16:06:35 +0800

    refactor(plugins): update ensure_installed tool order
    - Modified the order of tools in the ensure_installed list
    - Improved code clarity by grouping related tools together

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,53 +1,54 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-                width = 0.9,
-                border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
-            },
-        },
-    },
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        cmd = "MasonToolsInstall",
-        config = function()
-            local ensure_installed = {
-                -- C/C++
-                "clangd",
-                "cpplint",
-                "clang-format",
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+				width = 0.9,
+				border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		cmd = "MasonToolsInstall",
+		config = function()
+			local ensure_installed = {
+				-- C/C++
+				"clangd",
+				"clang-format",
+				"cpplint",
 
-                -- Lua
-                "stylua",
-                "lua-language-server",
+				-- Lua
+				"lua-language-server",
+				"stylua",
 
-                -- Php
-                "intelephense",
-                "phpcs",
-                "php-cs-fixer",
+				-- Php
+				"intelephense",
+				"phpcs",
+				"php-cs-fixer",
 
-                -- Python
-                "ruff",
-                "isort",
-                "pyright",
-                "djlint", -- For web
+				-- Python
+				"black",
+				"djlint", -- For web
+				"isort",
+				"pyright",
+				"ruff",
 
-                -- Web Things
-                "css-lsp",
-                "emmet-language-server",
-                "eslint_d",
-                "html-lsp",
-                "prettierd",
-                "typescript-language-server",
-            }
-            require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-        end,
-    },
+				-- Web Things
+				"css-lsp",
+				"emmet-language-server",
+				"eslint_d",
+				"html-lsp",
+				"prettierd",
+				"typescript-language-server",
+			}
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+		end,
+	},
 }
