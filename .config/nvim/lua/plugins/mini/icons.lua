07665258125b33cf6ba7373a21d60e2852b0277c
John Christian Vicente <johnc.vicente1@gmail.com>
2025-09-24 02:51:43 +0800

    refactor(icons): mock nvim-web-devicons in mini.icons
    To avoid errors when nvim-web-devicons is not present, the module is now mocked within mini.icons. This ensures that the plugin functions correctly even without the external dependency.

--- .config/nvim/lua/plugins/mini/icons.lua
+++ .config/nvim/lua/plugins/mini/icons.lua
@@ -1,11 +1,10 @@
 return function()
-	require("mini.icons").setup({
-		file = {
-			[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
-			["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
-		},
-		filetype = {
-			dotenv = { glyph = "", hl = "MiniIconsYellow" },
-		},
-	})
+	local MiniIcons = require("mini.icons")
+
+	MiniIcons.setup()
+
+	package.preload["nvim-web-devicons"] = function()
+		MiniIcons.mock_nvim_web_devicons()
+		return package.loaded["nvim-web-devicons"]
+	end
 end
