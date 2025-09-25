2501041d896fad784cd655ca04b474f755497e77
John Christian Vicente <johnc.vicente1@gmail.com>
2025-09-25 23:55:56 +0800

    feat: yey added nvim-jdtls and added ftplugin for java
    yey added java next c# lmao

---
+++ .config/nvim/ftplugin/java.lua
@@ -0,0 +1,80 @@
+local home = os.getenv("HOME")
+local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
+local status, jdtls = pcall(require, "jdtls")
+if not status then
+	return
+end
+
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
+
+vim.lsp.config("*", {
+	capabilities = vim.tbl_deep_extend("force", capabilities, {
+		textDocument = {
+			foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			},
+		},
+	}),
+	root_markers = { ".git" },
+})
+
+local config = {
+	cmd = {
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"-jar",
+		vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		"-configuration",
+		home .. "/.local/share/nvim/mason/packages/jdtls/config_mac",
+		"-data",
+		workspace_dir,
+	},
+	root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+	capabilities = capabilities,
+	settings = {
+		java = {
+			signatureHelp = { enabled = true },
+			extendedClientCapabilities = extendedClientCapabilities,
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			inlayHints = {
+				parameterNames = {
+					enabled = "all",
+				},
+			},
+			format = {
+				enabled = false,
+			},
+		},
+	},
+
+	init_options = {
+		bundles = {},
+	},
+}
+require("jdtls").start_or_attach(config)
