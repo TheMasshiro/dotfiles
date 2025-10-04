54e2550ebfdd2004874679ad54e7a1f73e5cefc7
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-04 19:55:22 +0800

    refactor: added more verticals

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -26,6 +26,8 @@ end, { desc = "Toggle inlay hints" })
 vim.keymap.set("n", "<leader>bd", "<CMD>bdelete<CR>", { silent = true, desc = "Close Current Buffer" })
 
 -- Create vertical windows
-vim.keymap.set("n", "\\v", "<CMD>vnew<CR>", { silent = true, desc = "Vertical split" })
+vim.keymap.set("n", "\\vs", "<CMD>vsplit<CR>", { silent = true, desc = "Vertical split" })
+vim.keymap.set("n", "\\vn", "<CMD>vnew<CR>", { silent = true, desc = "Vertical new" })
+vim.keymap.set("n", "\\vt", "<CMD>vsplit | terminal<CR>i", { silent = true, desc = "Vertical terminal" })
 vim.keymap.set("n", "<A-l>", "<CMD>vertical resize +5<CR>", { silent = true, desc = "Increase split width" })
 vim.keymap.set("n", "<A-h>", "<CMD>vertical resize -5<CR>", { silent = true, desc = "Decrease split width" })
