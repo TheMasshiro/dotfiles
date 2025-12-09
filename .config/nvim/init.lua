8cc4ce97ba46d5f87bda2b334b67ca2cc8ae8369
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-09 19:24:44 +0800

    feat(ui): introduce advanced visual feedback and structured navigation tools

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,26 @@ require("config.autocmd")
 require("config.keymaps")
 require("config.lsp")
 
-require("scripts.beacon").setup()
-require("scripts.diagnostics").setup()
-require("scripts.highlights").setup()
+-- `LSP hover`, `Diagnostics`, `Quickfix`, `Beacon` etc. should be loaded first.
 require("scripts.lsp_hover").setup()
+require("scripts.diagnostics").setup()
+require("scripts.quickfix").setup()
+require("scripts.beacon").setup()
+
+-- Load scripts that rely on `plugin`s.
+require("scripts.highlights").setup()
+require("scripts.color_sync")
+require("scripts.tree").setup()
+require("scripts.ts_regen").setup()
+
+require("scripts.color").setup()
+
+-- vim.api.nvim_create_autocmd({
+-- 	"VimEnter",
+-- 	"ColorScheme",
+-- }, {
+-- 	callback = function()
+-- 		_G.disable_properties()
+-- 		require("scripts.highlights").setup()
+-- 	end,
+-- })
