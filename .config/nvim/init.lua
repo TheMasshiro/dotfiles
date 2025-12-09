15aa579ce0a192bdbf57bd26591fca10dd00c6cc
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-09 19:38:08 +0800

    feat!: remove inline color preview and terminal synchronization

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,18 +11,5 @@ require("scripts.beacon").setup()
 
 -- Load scripts that rely on `plugin`s.
 require("scripts.highlights").setup()
-require("scripts.color_sync")
 require("scripts.tree").setup()
 require("scripts.ts_regen").setup()
-
-require("scripts.color").setup()
-
--- vim.api.nvim_create_autocmd({
--- 	"VimEnter",
--- 	"ColorScheme",
--- }, {
--- 	callback = function()
--- 		_G.disable_properties()
--- 		require("scripts.highlights").setup()
--- 	end,
--- })
