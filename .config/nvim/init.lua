266c7a7047605f9fe7a3c5071c9384c87becb64e
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-09 18:39:20 +0800

    feat(popup): Add adaptive LSP hover popup with custom borders

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,7 @@ require("config.autocmd")
 require("config.keymaps")
 require("config.lsp")
 
+require("scripts.beacon").setup()
+require("scripts.diagnostics").setup()
+require("scripts.highlights").setup()
+require("scripts.lsp_hover").setup()
