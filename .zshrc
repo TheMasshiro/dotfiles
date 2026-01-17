1be4e10831a2ae5c8342cddc7a4b237e09ca1129
John Christian Vicente <johnc.vicente1@gmail.com>
2026-01-17 12:41:07 +0800

    feat(config): streamline shell environment with new utilities and refined theming

--- .zshrc
+++ .zshrc
@@ -1,3 +1,5 @@
+fastfetch
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -86,6 +88,7 @@ alias cheat="~/bin/chtfzf.sh"
 alias update_all="~/bin/autoupdate.sh"
 alias ls="eza --icons=always"
 alias lg="lazygit"
+alias vim="nvim"
 
 
 # Shell integrations
--- theme.zsh
+++ theme.zsh
@@ -1,4 +1,9 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#222436,bg:#222436,spinner:#89ddff,hl:#f5c2e7 \
---color=fg:#c8d3f5,header:#82aaff,info:#7dcfff,pointer:#82aaff \
---color=marker:#86e1fc,fg+:#c8d3f5,prompt:#89ddff,hl+:#f2cdcd"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#a6e3a1,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+# export FZF_DEFAULT_OPTS=" \
+# --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+# --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+# --color=marker:#40a02b,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
