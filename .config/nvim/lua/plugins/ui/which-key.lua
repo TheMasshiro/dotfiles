191061bf63ecd32e6a7b23a7273d33c28c362015
John Christian Vicente <johnc.vicente1@gmail.com>
2026-06-29 18:01:33 +0800

    refactor(plugins): streamline which-key.nvim definition for lazy.nvim

--- .config/nvim/lua/plugins/ui/which-key.lua
+++ .config/nvim/lua/plugins/ui/which-key.lua
@@ -1,32 +1,31 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-	{
-		-- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "vimEnter",
-		---@module 'which-key'
-		---@type wk.Opts
-		---@diagnostic disable-next-line: missing-fields
-		opts = {
-			preset = "modern",
-			-- Delay between pressing a key and opening which-key (milliseconds)
-			delay = 0,
-			-- Disable
-			disable = {
-				ft = { "alpha" },
-			},
-			icons = { mappings = vim.g.have_nerd_font },
-			-- Document existing key chains
-			spec = {
-				{ "<leader>b", group = "Buffer" },
-				{ "<leader>g", group = "Git Hunk", mode = { "n", "v" } },
-				{ "<leader>h", group = "Harpoon", icon = "󰛢" },
-				{ "<leader>hl", group = "Lazy", mode = { "n", "v" } },
-				{ "<leader>f", group = "Find", mode = { "n", "v" } },
-				{ "<leader>t", group = "Toggle" },
-				{ "gr", group = "LSP Actions", mode = { "n" } },
-			},
+	-- Useful plugin to show you pending keybinds.
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	---@module 'which-key'
+	---@type wk.Opts
+	---@diagnostic disable-next-line: missing-fields
+	opts = {
+		preset = "modern",
+		-- Delay between pressing a key and opening which-key (milliseconds)
+		delay = 0,
+		-- Disable
+		disable = {
+			ft = { "alpha" },
+		},
+		icons = { mappings = vim.g.have_nerd_font },
+		-- Document existing key chains
+		spec = {
+			{ "<leader>b", group = "Buffer" },
+			{ "<leader>d", group = "Debugger" },
+			{ "<leader>g", group = "Git Hunk", mode = { "n", "v" } },
+			{ "<leader>gl", group = "Lazy", mode = { "n", "v" } },
+			{ "<leader>h", group = "Harpoon", icon = "󰛢" },
+			{ "<leader>f", group = "Find", mode = { "n", "v" } },
+			{ "<leader>t", group = "Toggle" },
+			{ "gr", group = "LSP Actions", mode = { "n" } },
 		},
 	},
 }
