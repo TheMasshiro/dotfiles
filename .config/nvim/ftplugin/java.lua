fbae54f16496273ff6702fdad24a4394feec551a
John Christian Vicente <johnc.vicente1@gmail.com>
2026-06-29 17:18:04 +0800

    refactor(nvim): consolidate Java LSP keymaps with which-key

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -63,24 +63,47 @@ local config = {
 }
 require("jdtls").start_or_attach(config)
 
-vim.keymap.set("n", "<leader>co", "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = "Organize Imports" })
-vim.keymap.set("n", "<leader>crv", "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = "Extract Variable" })
-vim.keymap.set(
-	"v",
-	"<leader>crv",
-	"<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-	{ desc = "Extract Variable" }
-)
-vim.keymap.set("n", "<leader>crc", "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = "Extract Constant" })
-vim.keymap.set(
-	"v",
-	"<leader>crc",
-	"<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-	{ desc = "Extract Constant" }
-)
-vim.keymap.set(
-	"v",
-	"<leader>crm",
-	"<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
-	{ desc = "Extract Method" }
-)
+local wk = require("which-key")
+wk.add({
+	{ "<leader>c", group = "Code Actions", buffer = 0 },
+	{ "<leader>cr", group = "Refactor", buffer = 0 },
+	{
+		"<leader>co",
+		"<Cmd>lua require'jdtls'.organize_imports()<CR>",
+		desc = "Organize Imports",
+		buffer = 0,
+	},
+	{
+		"<leader>crv",
+		"<Cmd>lua require('jdtls').extract_variable()<CR>",
+		desc = "Extract Variable",
+		buffer = 0,
+	},
+	{
+		"<leader>crc",
+		"<Cmd>lua require('jdtls').extract_constant()<CR>",
+		desc = "Extract Constant",
+		buffer = 0,
+	},
+	{
+		"<leader>crv",
+		"<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
+		desc = "Extract Variable",
+		mode = "v",
+		buffer = 0,
+	},
+	{
+		"<leader>crc",
+		"<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
+		desc = "Extract Constant",
+		mode = "v",
+		buffer = 0,
+	},
+	{
+		"<leader>crm",
+		"<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
+		desc = "Extract Method",
+		mode = "v",
+		buffer = 0,
+	},
+})
